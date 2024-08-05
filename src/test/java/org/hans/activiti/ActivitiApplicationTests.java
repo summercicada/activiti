5c3868c474847d052514951b94a6a5d8df124217
breat <breatinsummer@163.com>
2024-08-06 00:29:14 +0800

    init

---
+++ src/test/java/org/hans/activiti/ActivitiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hans.activiti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
